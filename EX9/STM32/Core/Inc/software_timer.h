e4e075b7fa289efbcf8ac7a73ccca1978ce2570a
Tuấn Bảo Nguyễn <bao.nguyentuanbao09@hcmut.edu.vn>
2022-10-25 22:04:11 +0700

    EX9

---
+++ EX9/STM32/Core/Inc/software_timer.h
@@ -0,0 +1,19 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 9, 2022
+ *      Author: tuanb
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag;
+extern int timer1_flag;
+extern int timer2_flag;
+void setTimer0 (int duration);
+void setTimer1 (int duration);
+void setTimer2 (int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
